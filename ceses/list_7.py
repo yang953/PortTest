bdd336e1600d4c88094ff7d500eb172f1da324cc
yang953 <yang134953@163.com>
2020-07-30 00:12:32 +0800

    test

--- ceses/list_7.py
+++ ceses/list_7.py
@@ -3,13 +3,13 @@ import re
 def choice_email():
     while 1:
         email = input(r"请输入邮箱地址:")
-        if email=="Q" or "q":
+        if email=="Q" or email=="q":
             # break
             exit()
         elif re.match(r"^(\w+)*@(\w+)+(\.com)$",email):
-            user_name = re.findall("(\w+)*@",email)
-            company_name = re.findall("@(\w+).com$",email)
-            print("邮件中用户名称是:{0}，公司名称是:{1}".format(user_name,company_name))
+            user_name = re.findall("(\w+)*@",email)[0]
+            company_name = re.findall("@(\w+).com$",email)[0]
+            print("邮件中用户名称是:{0}\n公司名称是:{1}".format(user_name,company_name))
         else:
             print("incorrect email format")
 
@@ -23,3 +23,8 @@ if __name__=="__main__":
 # email_address = 'ddy_davie@aaa.com'
 # matched_address = re.match(pat, email_address)
 # print(matched_address.group())
+
+#
+# while 1:
+#     mail = input(r"请输入邮箱地址：")
+#     if mail = "Q"
---
+++ ceses/list_8.py
@@ -0,0 +1,14 @@
+# nums = [1,2,7,8,12,14,9,9,10,3]
+#
+# target = 15
+#
+# for i in nums:
+#     for j in nums:
+#         if i+j==target:
+#             print(nums.index(i))
+#
+# print(nums.index(1))
+
+def tow_num(num_list,target):
+
+    pass
